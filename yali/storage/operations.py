db256c8a4b070d362ca548b9af21157a74f21311
Mete Alpaslan <mete@pardus.org.tr>
2011-06-27 18:31:53 +0300

    Add requires and obsoletes methods to DeviceOperation classes.
    These establish a mechanism for resolving dependencies between
    operations.

--- yali/storage/operations.py
+++ yali/storage/operations.py
@@ -101,7 +101,7 @@ class DeviceOperation(object):
         if not isinstance(device, Device):
             raise ValueError("arg 1 must be a Device instance")
         self.device = device
-        # Establish a unique id for each action instance. Making shallow or
+        # Establish a unique id for each operation instance. Making shallow or
         # deep copyies of DeviceOperation instances will require __copy__ and
         # __deepcopy__ methods to handle incrementing the id in the copy
         self.id = DeviceOperation._id
@@ -160,6 +160,23 @@ class DeviceOperation(object):
                                  self.device.id)
         return s
 
+    def requires(self, operation):
+        """ Return True if self requires operation. """
+        return False
+
+    def obsoletes(self, operation):
+        """ Return True is self obsoletes operation.
+
+            DeviceAction instances obsolete other DeviceAction instances with
+            lower id and same device.
+        """
+        return (self.device.id == operation.device.id and
+                self.type == operation.type and
+                self.obj == operation.obj and
+                self.id > operation.id)
+
+
+
 class OperationCreateDevice(DeviceOperation):
     """ Operation representing the creation of a new device. """
     type = OPERATION_TYPE_CREATE
@@ -171,6 +188,31 @@ class OperationCreateDevice(DeviceOperation):
 
     def execute(self, intf=None):
         self.device.create(intf=intf)
+
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            Device create operations require other operations when either of the
+            following is true:
+
+                - this operation's device depends on the other operation's device
+                - both operations are partition create operations on the same disk
+                  and this partition has a higher number
+        """
+        rc = False
+        if self.device.dependsOn(operation.device):
+            rc = True
+        elif (operation.isCreate and operation.isDevice and
+              isinstance(self.device, Partition) and
+              isinstance(operation.device, Partition) and
+              self.device.disk == operation.device.disk):
+            # create partitions in ascending numerical order
+            selfNum = self.device.partedPartition.number
+            otherNum = operation.device.partedPartition.number
+            if selfNum > otherNum:
+                rc = True
+        return rc
+
 
 class OperationDestroyDevice(DeviceOperation):
     """ An operation representing the deletion of an existing device. """
@@ -189,6 +231,54 @@ class OperationDestroyDevice(DeviceOperation):
         # and returns it when we create a new device with the same name
         if self.device.partedDevice:
             self.device.partedDevice.removeFromCache()
+
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            Device destroy operations require other operations when either of the
+            following is true:
+
+                - the other operation's device depends on this operation's device
+                - both operations are partition create operations on the same disk
+                  and this partition has a lower number
+        """
+        rc = False
+        if operation.device.dependsOn(self.device):
+            rc = True
+        elif (operation.isDestroy and operation.isDevice and
+              isinstance(self.device, Partition) and
+              isinstance(operation.device, Partition) and
+              self.device.disk == operation.device.disk):
+            # remove partitions in descending numerical order
+            selfNum = self.device.partedPartition.number
+            otherNum = operation.device.partedPartition.number
+            if selfNum < otherNum:
+                rc = True
+        elif (operation.isDestroy and operation.isFormat and
+              operation.device.id == self.device.id):
+            # device destruction comes after destruction of device's format
+            rc = True
+        return rc
+
+    def obsoletes(self, operation):
+        """ Return True if self obsoletes operation.
+
+            - obsoletes all operations w/ lower id that act on the same device,
+              including self, if device does not exist
+
+            - obsoletes all but ActionDestroyFormat operations w/ lower id on the
+              same device if device exists
+        """
+        rc = False
+        if operation.device.id == self.device.id:
+            if self.id >= operation.id and not self.device.exists:
+                rc = True
+            elif self.id > operation.id and \
+                 self.device.exists and \
+                 not (operation.isDestroy and operation.isFormat):
+                rc = True
+
+        return rc
 
 
 class OperationResizeDevice(DeviceOperation):
@@ -217,6 +307,30 @@ class OperationResizeDevice(DeviceOperation):
     def cancel(self):
         self.device.targetSize = self.origsize
 
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            A device resize operation requires another operation if:
+
+                - the other operation is a format resize on the same device and
+                  both are shrink operations
+                - the other operation grows a device (or format it contains) that
+                  this operation's device depends on
+                - the other operation shrinks a device (or format it contains)
+                  that depends on this operation's device
+        """
+        retval = False
+        if operation.isResize:
+            if self.device.id == operation.device.id and \
+               self.dir == operation.dir and \
+               operation.isFormat and self.isShrink:
+                retval = True
+            elif operation.isGrow and self.device.dependsOn(operation.device):
+                retval = True
+            elif operation.isShrink and operation.device.dependsOn(self.device):
+                retval = True
+
+        return retval
 
 class OperationCreateFormat(DeviceOperation):
     """ An operation representing creation of a new filesystem. """
@@ -264,6 +378,33 @@ class OperationCreateFormat(DeviceOperation):
     def cancel(self):
         self.device.format = self.origFormat
 
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            Format create operation can require another operation if:
+
+                - this operation's device depends on the other operation's device
+                  and the other operation is not a device destroy operation
+                - the other operation is a create or resize of this operation's
+                  device
+        """
+        return ((self.device.dependsOn(operation.device) and
+                 not (operation.isDestroy and operation.isDevice)) or
+                (operation.isDevice and (operation.isCreate or operation.isResize) and
+                 self.device.id == operation.device.id))
+
+    def obsoletes(self, operation):
+        """ Return True if this operation obsoletes operation.
+
+            Format create operations obsolete the following operations:
+
+                - format operations w/ lower id on this operation's device
+        """
+        return (self.device.id == operation.device.id and
+                self.obj == operation.obj and
+                self.id > operation.id)
+
+
 
 class OperationDestroyFormat(DeviceOperation):
     """ An operation representing the removal of an existing filesystem.
@@ -294,6 +435,30 @@ class OperationDestroyFormat(DeviceOperation):
     def format(self):
         return self.origFormat
 
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            Format destroy operations require other operations when:
+
+                - the other operation's device depends on this operation's device
+                  and the other operation is a destroy operation
+        """
+        return operation.device.dependsOn(self.device) and operation.isDestroy
+
+    def obsoletes(self, operation):
+        """ Return True if this operation obsoletes operation.
+
+            Format destroy operations obsolete the following operations:
+
+            - format operations w/ lower id on same device, including self if
+              format does not exist
+        """
+        return (self.device.id == operation.device.id and
+                self.obj == self.obj and
+                (self.id > operation.id or
+                 self.id == operation.id and not self.device.exists))
+
+
 
 class OperationResizeFormat(DeviceOperation):
     """ An operation representing the resizing of an existing filesystem.
@@ -322,6 +487,32 @@ class OperationResizeFormat(DeviceOperation):
 
     def cancel(self):
         self.device.format.targetSize = self.origSize
+
+    def requires(self, operation):
+        """ Return True if self requires operation.
+
+            A format resize operation requires another operation if:
+
+                - the other operation is a device resize on the same device and
+                  both are grow operations
+                - the other operation shrinks a device (or format it contains)
+                  that depends on this operation's device
+                - the other operation grows a device (or format) that this
+                  operation's device depends on
+        """
+        retval = False
+        if operation.isResize:
+            if self.device.id == operation.device.id and \
+               self.dir == operation.dir and \
+               operation.isDevice and self.isGrow:
+                retval = True
+            elif operation.isShrink and operation.device.dependsOn(self.device):
+                retval = True
+            elif operation.isGrow and self.device.dependsOn(operation.device):
+                retval = True
+
+        return retval
+
 
 class OperationMigrateFormat(DeviceOperation):
     """ An operation representing the migration of an existing filesystem. """
