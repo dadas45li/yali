5d448affccfdb5a434648820ac97ef813234450a
Mete Alpaslan <mete@pardus.org.tr>
2011-06-28 10:10:16 +0300

    Remove some unused code from storage.library.devicemapper

--- yali/storage/library/devicemapper.py
+++ yali/storage/library/devicemapper.py
@@ -37,37 +37,3 @@ def dm_node_from_name(name):
     dm_node = "dm-%d" % int(minor)
     ctx.logger.debug("dm_node_from_name(%s) returning '%s'" % (name, dm_node))
     return dm_node
-
-def _get_backing_devnums_from_map(map_name):
-    ret = []
-    buf = yali.util.run_batch("dmsetup",["info", "--columns", "--noheadings",
-                              "-o", "devnos_used", map_name])[1]
-    dev_nums = buf.split()
-    for dev_num in dev_nums:
-        (major, colon, minor) = dev_num.partition(":")
-        ret.append((int(major), int(minor)))
-
-    return ret
-
-def get_backing_devnums(dm_node):
-    #dm_node = dm_node_from_name(map_name)
-    if not dm_node:
-        return None
-
-    top_dir = "/sys/block"
-    backing_devs = os.listdir("%s/%s/slaves/" % (top_dir, dm_node))
-    dev_nums = []
-    for backing_dev in backing_devs:
-        dev_num = open("%s/%s/dev" % (top_dir, backing_dev)).read().strip()
-        (_major, _minor) = dev_num.split(":")
-        dev_nums.append((int(_major), int(_minor)))
-
-    return dev_nums
-
-def get_backing_devs_from_name(map_name):
-    dm_node = dm_node_from_name(map_name)
-    if not dm_node:
-        return None
-
-    slave_devs = os.listdir("/sys/block/virtual/%s" % dm_node)
-    return slave_devs
